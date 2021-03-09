c51848ecd8eee44238b86e2c74477697b9e11715
juansesanchezl <juans.sanchezlopez@gmail.com>
2021-03-08 21:49:09 -0500

    Update auth.js

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -1,7 +1,7 @@
 const isLoggedIn = (req, res, next) => {
     console.log("Entro isLoggedIn");
     if (req.user) {
-      next(req,res);
+      next();
     } else {
       res.status(401).send('Not Logged In');
     }
